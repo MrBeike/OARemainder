6afb3182282027a30a7cc8a5a15f2b0133379463
CoatLee <lbbas@126.com>
2019-11-13 01:14:16 +0800

    增加ini文件,方便分发使用。解耦。

--- login.py
+++ login.py
@@ -2,72 +2,86 @@
 # -*-coding:utf-8-*-
 
 import requests
-from requests.cookies import RequestsCookieJar
 import time
+import configparser
+from requests.cookies import RequestsCookieJar
 from bs4 import BeautifulSoup
 from captcha_predict import main
 
-# 登录参数准备
-loginUrl = 'http://59.203.198.99:8081/uc-server/login'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0',
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Host': '59.203.198.99:8081',
-    'Referer': 'http://59.203.198.99:8081/uc-server/login'
-}
-# 系统代理（测试用）
-proxies = {
-    'http': '127.0.0.1:8888',
-    'https': '127.0.0.1:8888',
-}
 
+class OARemainder:
+    def __init__(self):
+        self.configPath = 'captcha/config.ini'
+        self.captchaPath = 'captcha/0000_00000000.jpg'
+        # 登录参数准备
+        self.loginUrl = 'http://59.203.198.99:8081/uc-server/login'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Host': '59.203.198.99:8081',
+            'Referer': 'http://59.203.198.99:8081/uc-server/login'
+        }
+        # 系统代理（测试用）
+        self.proxies = {
+            'http': '127.0.0.1:8888',
+            'https': '127.0.0.1:8888',
+        }
+        self.s = requests.session(headers=self.headers)
 
-# 验证码四位数字 去空格 识别为空处理[未完成]
-def captchaDownload(data):
-    with open('captcha/0000_00000000.jpg', 'wb') as fp:
-        fp.write(data)
-    return
+    # 读取用户配置信息
+    def config(self):
+        #  实例化configParser对象
+        config = configparser.ConfigParser()
+        # -read读取ini文件
+        config.read(self.configPath, encoding='utf-8')
+        # -get(section,option)得到section中option的值，返回为string类型
+        self.username = config.get('userInfo', 'username')
+        self.password = config.get('userInfo', 'password')
+        return
 
+    # 将验证码写入文件
+    def captchaDownload(self, data):
+        with open(self.capthchaPath, 'wb') as fp:
+            fp.write(data)
+        return
 
-def dataMaker(s):
-    # page = s.get(loginUrl)
-    # manual_cookies = RequestsCookieJar()
-    # manual_cookies.set('loginType', 'normal')
-    # print(page.status_code, s.cookies)
-    # s.cookies.update(manual_cookies)
-    # print(s.cookies)
-    # response = page.content
-    # soup = BeautifulSoup(response, "html.parser")
-    # lt = soup.find(name="input", attrs={"name": 'lt'}).get("value")
-    # execution = soup.find(name="input", attrs={"name": 'execution'}).get("value")
-    # imgUrl = 'http://59.203.198.99:8081/uc-server/imageServlet?now=%d' % (time.time() * 1000)
-    # imgcodeContent = s.get(imgUrl).content
-    # captchaDownload(imgcodeContent)
-    while True:
-        imgcode = main()
-        if len(str(imgcode)) == 4:
-            break
-        # 表单内容
-    datas = {
-        'lt': lt,
-        'execution': execution,
-        '_eventId': 'submit',
-        'platform': '',
-        'username': '370705',
-        'password': '050094',
-        'random': imgcode
-    }
-    return datas
+    # 准备登陆信息
+    def dataMaker(self):
+        s = self.s
+        page = s.get(self.loginUrl)
+        manual_cookies = RequestsCookieJar()
+        manual_cookies.set('loginType', 'normal')
+        print(page.status_code, s.cookies)
+        s.cookies.update(manual_cookies)
+        print(s.cookies)
+        response = page.content
+        soup = BeautifulSoup(response, "html.parser")
+        lt = soup.find(name="input", attrs={"name": 'lt'}).get("value")
+        execution = soup.find(name="input", attrs={"name": 'execution'}).get("value")
+        imgUrl = 'http://59.203.198.99:8081/uc-server/imageServlet?now=%d' % (time.time() * 1000)
+        imgcodeContent = s.get(imgUrl).content
+        self.captchaDownload(imgcodeContent)
+        while True:
+            imgcode = main()
+            if len(str(imgcode)) == 4:
+                break
+            # 表单内容
+        datas = {
+            'lt': lt,
+            'execution': execution,
+            '_eventId': 'submit',
+            'platform': '',
+            'username': self.username,
+            'password': self.password,
+            'random': imgcode
+        }
+        return datas
 
-
-def login():
-    s = requests.session()
-    s.headers = headers
-    dataMaker(s)
-
-
-# s.proxies = proxies
+    def login(self):
+        s = self.s
+        datas = self.dataMaker()
+        response = s.post(self.loginUrl, datas=datas).content
 
 
 if __name__ == '__main__':
-    login()
+    oa = OARemainder()
