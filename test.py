2dc708941b575eb6764627ee5c98d410d3fad20e
xeroxYor <lbbas@126.com>
2020-02-25 20:35:41 +0800

    update

--- test.py
+++ test.py
@@ -70,3 +70,28 @@ pw ='050094'
 print(hashlib.md5(pw.encode(encoding='utf-8')).hexdigest().upper())
 
 # http://59.203.198.22:8086/defaultroot/public/download/download.jsp?FileName=2020022419504463978337936.xlsx&verifyCode=F35DDE0D55FileName00&name=青阳县应对新冠肺炎疫情支持中小微企业责任清单.xlsx&path=govdocumentmanager
+
+
+import demjson
+
+str = "{result: 'success', data: {pager: {pageCount: 1, recordCount: 1}, data: [{'id': '379850306', 'documentSendFileByteNumber': '青科经信〔2019〕183号','documentSendFileTitle': '关于填报2019年中国声谷首台（套）创新产品免费示范应用计划的通知', 'documentSendFileWriteOrg': '节能与综合利用科','createdTime': '2019-11-13 00:00:00', 'createdEmp': '23409802', 'createdOrg': '23409770', 'sendFileLink': '','sendFileOverSee': '0', 'sendFileUserId': '379859013', 'goldGridId': '1573612387930', 'isReaded': '1','outSeeType': '', 'orgId': '23598744', 'documentWordType': '.doc', 'tableId': '23631466', 'handOutTime': '','sendToMyRange': '', 'recivedDate': '2019-11-13 10:54:50', 'empId': '23598786', 'isSyncToInfomation': '0','sendFileDraft': '刘自渊', 'accessoryName': '附件1 关于填报2019年中国声谷首台套创新产品免费示范应用计划的通知.pdf|附件2 示范应用计划申报表.xlsx','accessorySaveName': '2019111310323347757243457.pdf|2019111310323733651319778.xlsx', 'canDownload': '1','verifyCode1': '2A0479BD10FileName00', 'verifyCode': '766B2AFF46id00'}]}}"
+
+json = demjson.decode(str)
+
+if json['result'] == 'success':
+    json = json['data']
+    record = json['pager']['recordCount']
+    if record == 0:
+        msg = '无未读收文'
+    else:
+        msg = '未读收文{}封'.format(record)
+    print(msg)
+    docSet_info = json['data']
+
+
+    for i in range(record):
+        doc_info = docSet_info[i]
+        if doc_info['accessoryName'] != '':
+
+    print(doc_info_data)
+
